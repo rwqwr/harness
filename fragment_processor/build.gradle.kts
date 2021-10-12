2dbab569e7e817a7c09770f93e36b371dbde6e49
ivanov_r <ivanov.roman.b@gmail.com>
2021-10-12 21:34:52 +0300

    added ksp dependencies

--- fragment_processor/build.gradle.kts
+++ fragment_processor/build.gradle.kts
@@ -18,20 +18,15 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-
     implementation(dependency(JetBrains.Kotlin.Std))
     implementation(dependency(Google.Dagger.Api))
 
-    implementation("com.squareup:kotlinpoet:1.7.2")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.5.30-1.0.0")
+
+    implementation("com.squareup:kotlinpoet:1.10.1")
+    implementation("com.squareup:kotlinpoet-ksp:1.10.1")
+    implementation("com.squareup:kotlinpoet-metadata:1.10.1")
     implementation("com.squareup:javapoet:1.11.1")
-    implementation("com.squareup:kotlinpoet-metadata:1.7.2")
     implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.2.0")
     implementation(project(":fragment_processor_api"))
-    implementation(files("${System.getProperty("java.home")}/../lib/tools.jar"))
-
-    compileOnly("net.ltgt.gradle.incap:incap:0.3")
-    kapt("net.ltgt.gradle.incap:incap-processor:0.3")
-
-    compileOnly("com.google.auto.service:auto-service:1.0-rc4")
-    kapt("com.google.auto.service:auto-service:1.0-rc4")
 }
